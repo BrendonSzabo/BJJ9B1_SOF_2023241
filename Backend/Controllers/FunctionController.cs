0be48e6d2637e66d787d400d95c6dbcaae565f36
Nédli Péter <nedlipeter@stud.uni-obuda.hu>
2024-06-22 10:14:29 +0200

    added functioncontroller for scheduled tasks

---
+++ Backend/Controllers/FunctionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class FunctionController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("Valami----------");
+            return View();
+        }
+    }
+}
